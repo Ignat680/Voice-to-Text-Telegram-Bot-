0d72904d43b8538033052ce7fd191f07442e47dd
Ignat <koa.vnuk@gmail.com>
2026-02-03 11:41:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,3 +106,9 @@ if __name__ == '__main__':
     logger.info("Бот запущен!")
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
+if __name__ == '__main__':
+    main()
+       # 3. Запуск бота
+    logger.info("Бот запущен!")
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
