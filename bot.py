696a87dfbdec0e311ac9bdb69d8c52a2f29e0016
Ignat <koa.vnuk@gmail.com>
2026-02-19 12:25:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -103,5 +103,19 @@ def main() -> None:
 if __name__ == '__main__':
     main()
      
+----------
+         # 1. Создание объекта Application
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
-     
+    # 2. Добавление обработчиков
+    application.add_handler(CommandHandler("start", start_command))
+    
+    # Добавляем фильтр для голосовых сообщений
+    application.add_handler(MessageHandler(filters.Voice(), voice_to_text))
+
+    # 3. Запуск бота
+    logger.info("Бот запущен!")
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == '__main__':
+    main()
