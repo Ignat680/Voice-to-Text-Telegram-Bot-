def2d2335b520071a926ff8043ff8147bed08f11
Ignat <koa.vnuk@gmail.com>
2026-02-27 08:54:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -105,3 +105,15 @@ if __name__ == '__main__':
      
 ----------
 
+    application.add_handler(CommandHandler("start", start_command))
+    
+    # Добавляем фильтр для голосовых сообщений
+    application.add_handler(MessageHandler(filters.Voice(), voice_to_text))
+
+    # 3. Запуск бота
+    logger.info("Бот запущен!")
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == '__main__':
+    main()
+    
